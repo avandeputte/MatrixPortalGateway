6089dda7d47f7be3d44cb5e7a6aa00051ba3bc84
Alex Van de Putte <alex@vndptt.com>
2026-07-31 23:37:54 -0400

    Advertise 'alarms' alongside 'timer' in capabilities -- both features individually discoverable

--- src/web.cpp
+++ src/web.cpp
@@ -812,7 +812,7 @@ static esp_err_t handleApiCapabilities(httpd_req_t* r) {
     snprintf(ft, sizeof(ft),
              "\"features\":[\"cells\",\"colors\",\"index\",\"lowercase\",\"pictographs\","
              "\"quiet\",\"ota\",\"canvas\",\"effects\",\"ticker\",\"brightness\",\"events\","
-             "\"effectDefs\",\"timer\"%s%s%s%s]}",
+             "\"effectDefs\",\"timer\",\"alarms\"%s%s%s%s]}",
              audioAvailable() ? ",\"audio\"" : "",
              soundAvailable() ? ",\"sound\"" : "",
              sensorAvailable() ? ",\"environment\"" : "",
